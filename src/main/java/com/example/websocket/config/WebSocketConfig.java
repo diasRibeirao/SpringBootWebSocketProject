f21b99bc286c76c3ba9bda074f0372445ae223ad
Emerson de Oliveira <emersondiaspd@gmail.com>
2024-12-09 21:00:23 -0300

    commit

--- src/main/java/com/example/websocket/config/WebSocketConfig.java
+++ src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -12,12 +12,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        // topic é o prefixo usado para os destinos em que os clientes podem se inscrever para receber atualizações em tempo real.
+        // Por exemplo, um cliente pode se inscrever no tópico /topic/approvals para receber mensagens.
         config.enableSimpleBroker("/topic");
+        // Habilita o Simple Broker:
+
+
+        // Isso significa que as mensagens enviadas pelo cliente ao servidor terão o prefixo /app e serão roteadas ao controller correto.
+        // Por exemplo:
+        // Cliente envia mensagem para /app/approvals
+        // O servidor a processa e publica no /topic/approvals
         config.setApplicationDestinationPrefixes("/app");
+
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Define o endpoint /ws para os clientes STOMP se conectarem ao servidor WebSocket.
+        // Permite origens de qualquer lugar (para testes e desenvolvimento).
+        // Em Prod: .setAllowedOriginPatterns("https://example.com") // Restringe a origem específica
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
